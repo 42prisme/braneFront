8089bc3b512bf73bcb6467c959e831558f5b5c17
v42 <hugo.molle@protonmail.com>
2021-03-07 22:17:30 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JalonListComponent } from './jalon-list/jalon-list.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 
 const routes: Routes = [
   {
     path:"projectList",
     component: ProjectListComponent
+  },
+  {
+    path:"JalonList",
+    component: JalonListComponent
   },
   {
     path:"",
--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -13,23 +13,22 @@ import { Observable } from 'rxjs';
 export class ProjectListComponent implements OnInit {
 
   projects: ProjectEntity[] = [];
+  isPossible: Observable<UserEntity> = new Observable<UserEntity>();
   constructor(private project_service: ProjectService, private user_service: UserService) { }
 
   ngOnInit(): void {
     this.project_service.GetAllProjects().subscribe( val => {
       this.projects = val;
+      this.populateUserEntity();
     });
   }
 
-  // public populateUserEntity(): void {
-  //   this.user_service.GetUser(id).subscribe( val => {
-  //     console.log(val.name);
-  //     return val.name;
-  //   });
-  // }
-
-  public getUser(id: number): Observable<UserEntity> {
-    return this.user_service.GetUser(id).pipe()
+  public populateUserEntity(): void {
+    this.projects.forEach(element => {
+      this.user_service.GetUser(element.assigneeId).subscribe( val => {
+        element.assignee = val;
+        console.log(val.name);
+      });
+    });
   }
-
 }
